534bbf7489d78988e56332a288d2bd363deb28cb
OksanaOriflame <emr201000@gmail.com>
2022-11-13 04:31:37 +0500

    task done

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,6 +1,7 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Film from '../../types/film';
+import VideoPlayer from '../video-player/video-player';
 
 type Props = {
   film: Film;
@@ -8,13 +9,39 @@ type Props = {
 };
 
 const FilmCard: FC<Props> = ({ film, onMouseOver }) => {
-  const { name, previewImage, id } = film;
-  const handleMouseOver = () => onMouseOver(film);
+  const { name, id } = film;
+  const [isPlay, setIsPlay] = useState(false);
+  const [isStartPlay, setIsStartPlay] = useState(false);
+
+  useEffect(() =>{
+    let playState = true;
+
+    if (isStartPlay)
+    {
+      setTimeout(() => {
+        setIsPlay(playState);
+      }, 1000);
+    }
+
+    return () => {
+      playState = false;
+    };
+  }, [isStartPlay]);
+
+  const handleMouseOver = () => {
+    setIsStartPlay(true);
+    onMouseOver(film);
+  };
+
+  const handleMouseLeave = () => {
+    setIsPlay(false);
+    setIsStartPlay(false);
+  };
 
   return (
-    <article className="small-film-card catalog__films-card" onMouseOver={handleMouseOver}>
+    <article className="small-film-card catalog__films-card" onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
       <div className="small-film-card__image">
-        <img src={previewImage} alt={name} width={280} height={175} />
+        <VideoPlayer film={film} isPlay={isPlay} width={280} height={175} />
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${id}`}>{name}</Link>
